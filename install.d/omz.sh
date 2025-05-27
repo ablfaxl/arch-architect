b300bffdab2c94e27e46199a5e9599edbe3d6980
Abolfazl Taghavi <taghavi.a2023@gmail.com>
2025-05-27 14:00:50 +0330

    feat: add script to install Oh-My-Zsh and plugins

---
+++ install.d/omz.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+progress "Installing Oh-My-Zsh and plugins..."
+
+pacman -S --noconfirm zsh git
+
+if [[ ! -d ~/.oh-my-zsh ]]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+
+progress "Oh-My-Zsh installed."
